d558cf60ac19c8fd9af13bef692bd5b6256cbc26
Tracey Bullington <traceylbullington@gmail.com>
2023-06-08 22:36:37 -0500

    Initial Commit

---
+++ ToDoLast/ToDoLastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoLastApp.swift
+//  ToDoLast
+//
+//  Created by scholar on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoLastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
